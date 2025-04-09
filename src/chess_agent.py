388c55677830c788626c6f25807ebce123aaa871
Stepan Kovar <kovar.stepan@protonmail.com>
2025-04-09 02:15:33 +0200

    implemented replay buffer

--- src/chess_agent.py
+++ src/chess_agent.py
@@ -32,6 +32,50 @@ parser.add_argument("--total_decay_iterations", default=100, type=int, help="Tot
 parser.add_argument("--board_size", default=7, type=int, help="Board size.")
 parser.add_argument("--num_actions", default=28, type=int, help="Number of actions.")
 
+class ReplayBuffer:
+    """Simple replay buffer with possibly limited capacity."""
+    def __init__(self, max_length=None):
+        self._max_length = max_length
+        self._data = []
+        self._offset = 0
+
+    def __len__(self):
+        return len(self._data)
+
+    @property
+    def max_length(self):
+        return self._max_length
+
+    def append(self, item):
+        if self._max_length is not None and len(self._data) >= self._max_length:
+            self._data[self._offset] = item
+            self._offset = (self._offset + 1) % self._max_length
+        else:
+            self._data.append(item)
+
+    def extend(self, items):
+        if self._max_length is None:
+            self._data.extend(items)
+        else:
+            for item in items:
+                if len(self._data) >= self._max_length:
+                    self._data[self._offset] = item
+                    self._offset = (self._offset + 1) % self._max_length
+                else:
+                    self._data.append(item)
+
+    def __getitem__(self, index):
+        assert -len(self._data) <= index < len(self._data)
+        return self._data[(self._offset + index) % len(self._data)]
+
+    def sample(self, size, generator=np.random, replace=True):
+        # By default, the same element can be sampled multiple times. Making sure the samples
+        # are unique is costly, and we do not mind the duplicites much during training.
+        if replace:
+            return [self._data[index] for index in generator.randint(len(self._data), size=size)]
+        else:
+            return [self._data[index] for index in generator.choice(len(self._data), size=size, replace=False)]
+
 def adjust_learning_rate(optimizer, iteration, args):
     lr = args.learning_rate - (args.learning_rate - args.final_learning_rate) * (iteration / args.total_decay_iterations)
     for param_group in optimizer.param_groups:
